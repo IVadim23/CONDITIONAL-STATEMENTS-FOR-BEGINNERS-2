c01213864590e1501bee1eae56b94cfbe4cfcf5f
IVadim23 <inazrski@gmail.com>
2024-08-18 01:34:18 +0300

    Add files via upload

---
+++ 02. Greater Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02._Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numer1 = int.Parse(Console.ReadLine());
+            int numer2 = int.Parse(Console.ReadLine());
+
+            if (numer1 >= numer2)
+            {
+                Console.WriteLine(numer1);
+            }
+            else
+            {
+                Console.WriteLine(numer2);
+            }
+
+        }
+    }
+}
